e872ad427beb046ce29b71f11094fe4b754fed25
Arun S. Negi <arunsnegi@protonmail.com>
2024-03-15 23:44:10 +0530

    bash: add fzf key-binding

---
+++ bash/.bash.d/fzf.sh
@@ -0,0 +1,7 @@
+# fzf
+
+# https://github.com/junegunn/fzf
+# for the linux install
+if [ -f "/usr/share/doc/fzf/examples/key-bindings.bash" ]; then
+    . "/usr/share/doc/fzf/examples/key-bindings.bash"
+fi
